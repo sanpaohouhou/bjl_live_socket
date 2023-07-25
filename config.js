791f442ec3e06a782e83cf514a5a8ecff8429377
wyx <tomwang916@163.com>
2023-07-25 11:33:11 +0800

    first

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	'REDISHOST' : 'development.hej4hw.ng.0001.apne1.cache.amazonaws.com',
+	'REDISPASS' : '',
+	'REDISPORT' : '6379',
+	'REDISDB' 	: '7',
+	'TOKEN'		: '1234567',
+	'sign_key'	: '76576076c1f5f657b634e966c8836a06',
+	'socket_port': '14427',
+	'WEBADDRESS': 'http://bjl.dn28.link/api/'
+}
